88a0019a9d41c8514ae99683db1979b6551162a6
Eltayeb <elt.abdelrahman@gmail.com>
2019-12-24 12:22:40 -0500

    Version 2
    Add Speed
    Removed two buttons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,31 +1,40 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.PermissionChecker
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Integer.parseInt
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lm =
+            this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+//        check for permission access
+//        then we request update as frequently as possible
+
+        if (PermissionChecker.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+
+            onLocationChanged(null)
+        }
     }
 
-    fun toastMe(view: View) {
-        // Toast myToast = Toast.makeText(this, message, duration);
-        val myToast = Toast.makeText(
-            this, "Hello Rosey!",
-            Toast.LENGTH_SHORT
-        )
-        myToast.show()
-    }
-
-    fun countMe (view: View){
+//    count up
+    fun countUp (view: View){
 //        get the text view
         val showCountTextView: TextView =  findViewById(R.id.textView)
 
@@ -41,26 +50,57 @@ class MainActivity : AppCompatActivity() {
         showCountTextView.setText(count.toString());
     }
 
-    private val TOTAL_COUNT = "total_count"
+//    count down
+    fun countDown (view: View){
+//        get the text view
+        val showCountTextView: TextView =  findViewById(R.id.textView)
 
-    fun randomMe (view: View){
-// Create an Intent to start the second activity
-        val randomIntent = Intent(this, SecondActivity::class.java)
-
-//        Get the text view that shows the count.
-        val showCountTextView : TextView = findViewById(R.id.textView)
-
-//        get the value of the text view
+//        get the value of the text view.
         val countString = showCountTextView.text.toString()
 
-//        convert the count to an int
-        val count = parseInt(countString)
+//        convert value to a number and increment it
 
-//        add the count to the extras for the intent.
-        randomIntent.putExtra(TOTAL_COUNT, count)
+        var count = parseInt(countString);
+        count--
 
-// Start the new activity.
-        startActivity(randomIntent)
-
+//        Display the new value in the text view.
+        showCountTextView.setText(count.toString());
     }
+
+    private val TOTAL_COUNT = "total_count"
+
+
+    //    locating and changing the text view with the correct speed
+
+    override fun onLocationChanged(location: Location?) {
+
+        val SpeedTextView: TextView =  findViewById(R.id.curSpeedView)
+
+        if( location == null ){
+            SpeedTextView.setText("--.-- m/s");
+        }
+        else{
+            val nCurrentSpeed = location.getSpeed();
+            SpeedTextView.setText("$nCurrentSpeed m/s")
+        }
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    // TO DO:
+//    add set button,
+//    connect music playlist,
+//    based on speed difference from target goal, play different kind os sub playlist
+
+
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -11,35 +11,7 @@ class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-        showRandomNumber()
     }
 
     private val TOTAL_COUNT = "total_count"
-
-    fun showRandomNumber(){
-
-//        get the text view where the random number will be displayed
-        val randomView:TextView = findViewById(R.id.textview_random)
-
-//      get the textview where the heading is displayed
-        val headingView:TextView = findViewById(R.id.textview_label)
-
-//        get the count from the intent extras
-        val count = intent.getIntExtra(TOTAL_COUNT, 0)
-
-//        Generate random number
-        val random = Random()
-        var randomInt = 0
-        if (count > 0)
-            randomInt = random.nextInt(count)
-
-//        Display the random number.
-        randomView.setText(Integer.toString(randomInt))
-
-//        substitute the max value into the string resource for the heading, and update heading
-        headingView.setText(getString(R.string.random_heading, count))
-
-
-    }
 }
